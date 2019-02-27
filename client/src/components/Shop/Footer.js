dba8fde7fad31875725da2d23554ba112c077c4d
pdhoward <patrick.howard@hotmail.com>
2019-02-27 13:00:58 -0500

    update links and references

--- client/src/components/Shop/Footer.js
+++ client/src/components/Shop/Footer.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 const Footer = props => {
   return (
     <footer>
       <p className="footer-links">
         <a
-          href="https://github.com/sivadass/react-shopping-cart"
+          href="https://github.com/pdhoward/botshop"
           target="_blank"
         >
           View Source on Github
         </a>
         <span> / </span>
-        <a href="mailto:contact@sivadass.in" target="_blank">
+        <a href="mailto:patrick.howard@xiollc.com" target="_blank">
           Need any help?
         </a>
         <span> / </span>
-        <a href="https://twitter.com/NSivadass" target="_blank">
-          Say Hi on Twitter
+        <a href="https://twitter.com/urban_coder" target="_blank">
+          Follow Urban Coder on Twitter
         </a>
         <span> / </span>
-        <a href="https://sivadass.in" target="_blank">
-          Read My Blog
+        <a href="https://www.linkedin.com/in/patrickhowardusa/" target="_blank">
+          Let's Connect
         </a>
       </p>
       <p>
-        &copy; 2017 <strong>Veggy</strong> - Organic Green Store
+        &copy; 2018 <strong>Strategic Machines</strong> - Conversational Economy
       </p>
     </footer>
   );
